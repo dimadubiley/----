31cefe0145cafc75985916fafd37e0408563134f
Дима Дубилей <dimon.pr.2007@gmail.com>
2024-09-09 16:12:10 +0300

    add resume by JS

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-document.write("Yes")
+document.write("Name: DD <br>");
+document.write("Age: 16+ <br>");
+document.write("Favorit: i love play minecraft <br>");
+document.write("favorit food: pizza <br>")
